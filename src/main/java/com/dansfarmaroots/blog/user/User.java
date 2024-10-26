b2793b56a45d9690b8fe13fe1ff3d7033b8293aa
chun <kchan307@my.centennialcollege.ca>
2024-10-26 17:06:17 -0400

    updated user

--- src/main/java/com/dansfarmaroots/blog/user/User.java
+++ src/main/java/com/dansfarmaroots/blog/user/User.java
@@ -1,7 +1,10 @@
 package com.dansfarmaroots.blog.user;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class User {
+    @Id
+    private String id;
 }
