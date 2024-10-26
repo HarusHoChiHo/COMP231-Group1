6cc2687b8e8ec26418784832f0ae63b1ed0966f7
Chi Ho Ho <harushochiho@gmail.com>
2024-10-26 17:48:40 -0400

    Added annotation to field 'author' and changed its data type

--- src/main/java/com/dansfarmaroots/blog/blog/Blog.java
+++ src/main/java/com/dansfarmaroots/blog/blog/Blog.java
@@ -1,14 +1,33 @@
 package com.dansfarmaroots.blog.blog;
 
+import com.dansfarmaroots.blog.user.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Document(collection = "blogs")
 public class Blog {
+    // Getters and Setters
     @Id
     private String id;
     private String title;
     private String content;
-    private String author; // or a User object reference
-    // Getters and Setters
+    @DBRef
+    private User author;
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", author=" + author +
+                '}';
+    }
 }
