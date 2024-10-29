f6dabdc99e2545490f8bc69162254aef30093aac
chun <kchan307@my.centennialcollege.ca>
2024-10-29 12:43:26 -0400

    Fixed Annotation

--- src/main/java/com/dansfarmaroots/blog/blog/BlogRepository.java
+++ src/main/java/com/dansfarmaroots/blog/blog/BlogRepository.java
@@ -1,7 +1,9 @@
 package com.dansfarmaroots.blog.blog;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends MongoRepository<Blog, String> {
     // Custom query methods can be defined here
 }
--- src/main/java/com/dansfarmaroots/blog/user/UserRepository.java
+++ src/main/java/com/dansfarmaroots/blog/user/UserRepository.java
@@ -1,9 +1,7 @@
 package com.dansfarmaroots.blog.user;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
 }
