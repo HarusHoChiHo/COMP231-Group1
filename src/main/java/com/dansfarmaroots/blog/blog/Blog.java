a9d1e1dcbef3b795a59ae1f77e1dfbcb85913d49
Chi Ho Ho <harushochiho@gmail.com>
2024-11-03 23:05:26 -0500

    Added publishDate to Blog

--- src/main/java/com/dansfarmaroots/blog/blog/Blog.java
+++ src/main/java/com/dansfarmaroots/blog/blog/Blog.java
@@ -1,6 +1,7 @@
 package com.dansfarmaroots.blog.blog;
 
 import com.dansfarmaroots.blog.user.User;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +9,9 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
+import java.sql.Date;
+
+@Data
 @NoArgsConstructor
 @Document(collection = "blogs")
 public class Blog {
@@ -18,16 +20,7 @@ public class Blog {
     private String id;
     private String title;
     private String content;
+    private Date publishDate;
     @DBRef
     private User author;
-
-    @Override
-    public String toString() {
-        return "Blog{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", author=" + author +
-                '}';
-    }
 }
