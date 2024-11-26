08cf01b0fe0fadc71ee16c81aa343b2a70d66ff4
matthewlau6c09 <matthewlau6c09@gmail.com>
2024-11-26 02:02:33 -0500

    minor Visual Change

--- src/app/pages/text_editor/[[...id]]/page.tsx
+++ src/app/pages/text_editor/[[...id]]/page.tsx
@@ -1,77 +1,83 @@
-"use client"
+"use client";
 
-import {LegacyRef, Suspense, useEffect, useRef, useState} from "react";
+import { LegacyRef, Suspense, useEffect, useRef, useState } from "react";
 import Editor from "@/components/ArticleManager";
 import Delta from "quill-delta";
 import "quill/dist/quill.snow.css";
 import Quill from "quill";
-import {HttpServices} from "@/lib/HttpServices";
-import {Blog, BlogCreation} from "@/lib/models/Blog";
-import {useAuth} from "@/app/AuthContext";
-import {useParams, useRouter} from "next/navigation";
+import { HttpServices } from "@/lib/HttpServices";
+import { Blog, BlogCreation } from "@/lib/models/Blog";
+import { useAuth } from "@/app/AuthContext";
+import { useParams, useRouter } from "next/navigation";
 import LoadingSpinner from "@/components/LoadingSpinner";
-import {jwtDecode} from "jwt-decode";
-
-
+import { jwtDecode } from "jwt-decode";
 
 export default function Page() {
     const httpService = new HttpServices();
     const [title, setTitle] = useState(""); // State for blog title
     const [text, setText] = useState("");
-    const [range, setRange] = useState();
-    //const [lastChange, setLastChange] = useState();
+    const [range, setRange] = useState(); //const [lastChange, setLastChange] = useState();
     const router = useRouter();
-    const params = useParams<{
-        id: string | undefined
-    }>();
+    const params = useParams<{ id: string | undefined }>();
     const [blog, setBlog] = useState<Blog>({
         authorName: "",
         content: "",
         id: "",
         publishDate: "",
-        title: ""
+        title: "",
     });
     const [isLoading, setIsLoading] = useState(true);
     const quillRef: LegacyRef<Quill> = useRef(null);
-    const {token} = useAuth();
-    const fontSizeArr = ['8px', '9px', '10px', '12px', '14px', '16px', '20px', '24px', '32px', '42px', '54px', '68px', '84px', '98px'];
-
-
-    const toolBarOptions = [
-        [{'header': [1, 2, 3, 4, 5, 6, false]}],
-        [{'size': ['small', false, 'large', 'huge']}],
-        ['bold', 'italic', 'underline', 'strike'],
-        ['blockquote', 'code-block'],
-        ['link', 'image', 'video', 'formula'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, {'list': 'check'}],
-        [{'script': 'sub'}, {'script': 'super'}],
-        [{'indent': '-1'}, {'indent': '+1'}],
-        [{'direction': 'rtl'}],
-        [{'color': []}, {'background': []}],
-        [{'font': []}],
-        [{'align': []}],
-        ['clean']
+    const { token } = useAuth();
+    const fontSizeArr = [
+        "8px",
+        "9px",
+        "10px",
+        "12px",
+        "14px",
+        "16px",
+        "20px",
+        "24px",
+        "32px",
+        "42px",
+        "54px",
+        "68px",
+        "84px",
+        "98px",
     ];
-
+    const toolBarOptions = [
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ size: ["small", false, "large", "huge"] }],
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote", "code-block"],
+        ["link", "image", "video", "formula"],
+        [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+        [{ script: "sub" }, { script: "super" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        [{ direction: "rtl" }],
+        [{ color: [] }, { background: [] }],
+        [{ font: [] }],
+        [{ align: [] }],
+        ["clean"],
+    ];
     const options = {
         debug: "error",
-        modules: {
-            toolbar: toolBarOptions
-        },
+        modules: { toolbar: toolBarOptions },
         placeholder: "Testing",
         readOnly: false,
-        theme: "snow"
+        theme: "snow",
     };
 
     useEffect(() => {
-
         try {
             if (!token) {
                 router.push("/pages/login");
             }
             if (params.id) {
                 (async () => {
-                    const responseBlog: Blog = await (await (httpService.callAPI(`/api/blogs/${params.id}`, null, "GET", token))).json();
+                    const responseBlog: Blog = await (
+                        await httpService.callAPI(`/api/blogs/${params.id}`, null, "GET", token)
+                    ).json();
                     setTitle(responseBlog.title);
                     setBlog(responseBlog);
                     setIsLoading(false);
@@ -93,7 +99,6 @@ export default function Page() {
         if (!userName) {
             return;
         }
-
         // Prepare blog data to send
         const blogData: BlogCreation = new BlogCreation(
             title, // Include title from state
@@ -101,46 +106,53 @@ export default function Page() {
             new Date().toUTCString(),
             userName
         );
-
-        httpService.callAPI("/api/blogs", blogData, "POST", token).then(response => {
-            console.log("Post created successfully:", response);
-            router.push("/pages/article_list");
-        }).catch(error => {
-            console.error("Error creating post:", error);
-        });
+        httpService
+            .callAPI("/api/blogs", blogData, "POST", token)
+            .then((response) => {
+                console.log("Post created successfully:", response);
+                router.push("/pages/article_list");
+            })
+            .catch((error) => {
+                console.error("Error creating post:", error);
+            });
     };
 
     const handleUpdatePost = () => {
         const content = JSON.stringify(quillRef.current?.getContents());
-
-        const {
+        const { id, publishDate, authorName } = blog;
+        const blogData: Blog = new Blog(
             id,
+            title,
+            content,
             publishDate,
             authorName
-        } = blog;
-
-        const blogData: Blog = new Blog(id, title, content, publishDate, authorName);
-
-        httpService.callAPI("/api/blogs", blogData, "POST", token).then(response => {
-            console.log("Post created successfully:", response);
-            router.push("/pages/article_list");
-        }).catch(error => {
-            console.error("Error creating post:", error);
-        });
-    }
+        );
+        httpService
+            .callAPI("/api/blogs", blogData, "POST", token)
+            .then((response) => {
+                console.log("Post updated successfully:", response);
+                router.push("/pages/article_list");
+            })
+            .catch((error) => {
+                console.error("Error updating post:", error);
+            });
+    };
 
     const handleCancel = () => {
         if (confirm("Would you like to cancel? All content will be lost.")) {
             router.push("/pages/article_list");
         }
-    }
+    };
 
     if (isLoading) {
-        return <LoadingSpinner/>;
+        return <LoadingSpinner />;
     }
 
-    return !isLoading && (
-        <div className={"d-flex justify-items-center min-h-[100vh] bg-white"}>
+    return (
+        <div
+            className={"d-flex justify-items-center min-h-[100vh] bg-white"}
+            style={{ marginBottom: 0, paddingBottom: 0 }}
+        >
             <div
                 className={
                     "w-full max-w-[800px] px-2.5 pt-4 text-[#333] font-[Arial, sans-serif]"
@@ -186,9 +198,11 @@ export default function Page() {
                 <div
                     style={{
                         display: "flex",
-                        justifyContent: "flex-end", // Align buttons to the right
+                        justifyContent: "flex-end",
+                        // Align buttons to the right
                         marginTop: "10px",
                         gap: "10px", // Space between buttons
+                        marginBottom: "50px", // Add margin from the bottom
                     }}
                 >
                     <button
@@ -217,7 +231,6 @@ export default function Page() {
                             cursor: "pointer",
                             width: "fit-content",
                             minWidth: "110px",
-
                         }}
                     >
                         Cancel
