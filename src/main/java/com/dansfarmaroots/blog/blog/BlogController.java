6fecbc6ae38d6ad67d040a4f6461ff179f87e948
chun <kchan307@my.centennialcollege.ca>
2024-11-26 13:27:10 -0500

    Don't need to push the username anymore.

--- src/main/java/com/dansfarmaroots/blog/blog/BlogController.java
+++ src/main/java/com/dansfarmaroots/blog/blog/BlogController.java
@@ -1,8 +1,13 @@
 package com.dansfarmaroots.blog.blog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/blogs")
 public class BlogController {
+    private static final Logger log = LoggerFactory.getLogger(BlogController.class);
     @Autowired
     private BlogService blogService;
 
@@ -25,6 +31,8 @@ public class BlogController {
 
     @PostMapping
     public ResponseEntity<Blog> createBlog(@RequestBody Blog blog) {
+        UserDetails details = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        blog.setAuthorName(details.getUsername());
         return new ResponseEntity<>(blogService.createBlog(blog), HttpStatus.CREATED);
     }
 
